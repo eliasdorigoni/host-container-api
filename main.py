0dcef7c1342bb3968e734fefedbf7095a31b678c
Elias Dorigoni <eliasdorigoni@gmail.com>
2025-02-01 19:17:25 -0300

    Unifies commands under a subdirectory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, status, Response
+from lib.HostContainerService import HostContainerService
 
 app = FastAPI()
 
@@ -8,35 +9,14 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/status", status_code=status.HTTP_200_OK)
-def read_status(response: Response):
-    from lib.HostContainerService import HostContainerService
-    try:
-        return HostContainerService("status").run_action()
-    except FileNotFoundError as e:
-        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
-        return {
-            "success": False,
-            "error": str(e)
-        }
-    except BrokenPipeError as e:
-        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
-        return {
-            "success": False,
-            "message": "Broken pipe",
-            "error": str(e)
-        }
-
-
-@app.get("/get-{action_name}", status_code=status.HTTP_200_OK)
+@app.get("/commands/{action_name}", status_code=status.HTTP_200_OK)
 def read_get_from_host(action_name: str, response: Response):
-    from lib.HostContainerService import HostContainerService
-    service = HostContainerService("get-" + action_name)
+    service = HostContainerService(action_name)
     if not service.is_existing_command:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {
             "success": False,
-            "message": "command not found: get-" + action_name,
+            "message": "Command not found: " + action_name,
         }
 
     try:
