659b05f273c822cd4bfe08e1ec037c93d8d783b8
Elias Dorigoni <eliasdorigoni@gmail.com>
2025-02-22 23:38:27 -0300

    Response uses a JSON string

--- lib/ResponseFormatter.py
+++ lib/ResponseFormatter.py
@@ -1,17 +1,20 @@
+import json
+
+
 class ResponseFormatter:
     def __init__(self, content=None):
         self.content = content
 
     def as_success(self, optional_message: str = None):
-        return {
+        return json.dumps({
             "success": True,
             "message": optional_message,
             "data": self.content,
-        }
+        })
 
     def as_error(self, optional_message: str = None):
-        return {
+        return json.dumps({
             "success": False,
             "message": optional_message,
             "data": self.content,
-        }
+        })
